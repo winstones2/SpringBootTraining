8e94b2753067925c7d94b8c6e59edf8a67133d48
tangyu <tangyu@crscd.com.cn>
2020-12-02 15:22:07 +0800

    1.删除了不必要的文件 2.调整了配置文件

--- src/main/java/com/tangyu/springboottraining/SpringBootTrainingApplication.java
+++ src/main/java/com/tangyu/springboottraining/SpringBootTrainingApplication.java
@@ -1,9 +1,11 @@
 package com.tangyu.springboottraining;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAutoConfiguration
 public class SpringBootTrainingApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/tangyu/springboottraining/SpringboottrainingApplicationTests.java
+++ src/test/java/com/tangyu/springboottraining/SpringboottrainingApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringboottrainingApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
